5d892a75e9ce57feb89b8b8552405b0a1a6e48a1
Udo Bretz <udo.bretz@otrs.com>
2011-12-19 16:19:54 +0000

    Updated polish translation, thanks to Pawel at Informatyka Boguslawski!

--- Kernel/Language/pl_ITSMCore.pm
+++ Kernel/Language/pl_ITSMCore.pm
@@ -2,7 +2,7 @@
 # Kernel/Language/pl_ITSMCore.pm - translation file
 # Copyright (C) 2001-2011 OTRS AG, http://otrs.org/
 # --
-# $Id: pl_ITSMCore.pm,v 1.15 2011-03-03 18:41:54 ub Exp $
+# $Id: pl_ITSMCore.pm,v 1.15.2.1 2011-12-19 16:19:54 ub Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -17,73 +17,73 @@ sub Data {
     my $Self = shift;
 
     # Template: AAAITSMCore
-    $Self->{Translation}->{'Alternative to'} = '';
+    $Self->{Translation}->{'Alternative to'} = 'Alternatywa dla';
     $Self->{Translation}->{'Availability'} = 'Dostêpno¶æ';
-    $Self->{Translation}->{'Back End'} = '';
+    $Self->{Translation}->{'Back End'} = 'Backend';
     $Self->{Translation}->{'Connected to'} = 'Pod³±czone do';
     $Self->{Translation}->{'Current State'} = 'Aktualny stan';
     $Self->{Translation}->{'Demonstration'} = 'Demonstracja';
     $Self->{Translation}->{'Depends on'} = 'Zale¿ne od';
-    $Self->{Translation}->{'End User Service'} = '';
+    $Self->{Translation}->{'End User Service'} = 'Us³uga u¿ytkownika koñcowego';
     $Self->{Translation}->{'Errors'} = 'B³êdy';
-    $Self->{Translation}->{'Front End'} = '';
-    $Self->{Translation}->{'IT Management'} = '';
-    $Self->{Translation}->{'IT Operational'} = '';
-    $Self->{Translation}->{'Impact'} = '';
+    $Self->{Translation}->{'Front End'} = 'Frontend';
+    $Self->{Translation}->{'IT Management'} = 'IT zarz±dzanie';
+    $Self->{Translation}->{'IT Operational'} = 'IT operacyjne';
+    $Self->{Translation}->{'Impact'} = 'Wp³yw';
     $Self->{Translation}->{'Incident State'} = 'Stan zdarzenia';
     $Self->{Translation}->{'Includes'} = 'Zawiera';
     $Self->{Translation}->{'Other'} = 'Inne';
-    $Self->{Translation}->{'Part of'} = '';
+    $Self->{Translation}->{'Part of'} = 'Czê¶æ';
     $Self->{Translation}->{'Project'} = 'Projekt';
     $Self->{Translation}->{'Recovery Time'} = 'Czas odzyskania';
-    $Self->{Translation}->{'Relevant to'} = '';
+    $Self->{Translation}->{'Relevant to'} = 'Odpowiednie do';
     $Self->{Translation}->{'Reporting'} = 'Raportowanie';
     $Self->{Translation}->{'Required for'} = 'Potrzebne do';
     $Self->{Translation}->{'Resolution Rate'} = 'Czas rozwi±zania';
     $Self->{Translation}->{'Response Time'} = 'Czas odpowiedzi';
-    $Self->{Translation}->{'SLA Overview'} = '';
-    $Self->{Translation}->{'Service Overview'} = '';
+    $Self->{Translation}->{'SLA Overview'} = 'Przegl±d SLA';
+    $Self->{Translation}->{'Service Overview'} = 'Przegl±d us³ugi';
     $Self->{Translation}->{'Service-Area'} = 'Sekcja serwisowa';
     $Self->{Translation}->{'Training'} = 'Trening';
     $Self->{Translation}->{'Transactions'} = 'Transakcje';
     $Self->{Translation}->{'Underpinning Contract'} = '';
-    $Self->{Translation}->{'allocation'} = '';
+    $Self->{Translation}->{'allocation'} = 'alokacja';
 
     # Template: AdminITSMCIPAllocate
-    $Self->{Translation}->{'Criticality <-> Impact <-> Priority'} = '';
-    $Self->{Translation}->{'Manage the priority result of combinating Criticality <-> Impact.'} = '';
-    $Self->{Translation}->{'Priority allocation'} = '';
+    $Self->{Translation}->{'Criticality <-> Impact <-> Priority'} = 'Krytyczno¶æ <-> Wp³yw <-> Priorytet';
+    $Self->{Translation}->{'Manage the priority result of combinating Criticality <-> Impact.'} = 'Zarz±dzanie warto¶ci± priorytetu dla kombinacji Krytyczno¶æ <-> Wp³yw.';
+    $Self->{Translation}->{'Priority allocation'} = 'Alokacja priorytetu';
 
     # Template: AdminSLA
     $Self->{Translation}->{'Minimum Time Between Incidents'} = 'Minimalny czas miêdzy zdarzeniami';
 
     # Template: AdminService
-    $Self->{Translation}->{'Criticality'} = '';
+    $Self->{Translation}->{'Criticality'} = 'Krytyczno¶æ';
 
     # Template: AgentITSMCustomerSearch
 
     # Template: AgentITSMSLA
 
     # Template: AgentITSMSLAPrint
-    $Self->{Translation}->{'SLA-Info'} = '';
-    $Self->{Translation}->{'Last changed'} = '';
-    $Self->{Translation}->{'Last changed by'} = '';
+    $Self->{Translation}->{'SLA-Info'} = 'SLA-Info';
+    $Self->{Translation}->{'Last changed'} = 'Ostatnia zmiana';
+    $Self->{Translation}->{'Last changed by'} = 'Ostatnio zmienione przez';
 
     # Template: AgentITSMSLAZoom
-    $Self->{Translation}->{'SLA Information'} = '';
-    $Self->{Translation}->{'Show or hide the content.'} = '';
+    $Self->{Translation}->{'SLA Information'} = 'Informacje SLA';
+    $Self->{Translation}->{'Show or hide the content.'} = 'Poka¿ ukryt± zawarto¶æ.';
     $Self->{Translation}->{'Associated Services'} = 'Po³±czone us³ugi';
 
     # Template: AgentITSMService
 
     # Template: AgentITSMServicePrint
-    $Self->{Translation}->{'Service-Info'} = '';
-    $Self->{Translation}->{'Current Incident State'} = 'Aktualny stan zdarzenia';
+    $Self->{Translation}->{'Service-Info'} = 'Us³uga-Info';
+    $Self->{Translation}->{'Current Incident State'} = 'Aktualny stan';
     $Self->{Translation}->{'Associated SLAs'} = 'Po³±czone SLA';
 
     # Template: AgentITSMServiceZoom
-    $Self->{Translation}->{'Service Information'} = '';
-    $Self->{Translation}->{'Current incident state'} = '';
+    $Self->{Translation}->{'Service Information'} = 'Informacje o us³udze';
+    $Self->{Translation}->{'Current incident state'} = 'Aktualny stan';
 
     # SysConfig
     $Self->{Translation}->{'Frontend module registration for the AdminITSMCIPAllocate configuration in the admin area.'} = '';
@@ -130,6 +130,12 @@ sub Data {
     $Self->{Translation}->{'This setting defines the link type \'Includes\'. If the source name and the target name contain the same value, the resulting link is a non-directional one. If the values are different, the resulting link is a directional link.'} = '';
     $Self->{Translation}->{'This setting defines the link type \'RelevantTo\'. If the source name and the target name contain the same value, the resulting link is a non-directional one. If the values are different, the resulting link is a directional link.'} = '';
     $Self->{Translation}->{'Width of ITSM textareas.'} = '';
+
+    #
+    # IB mods
+    #
+    $Self->{Translation}->{'Manage priority matrix.'} = 'Zarz±dzaj macierz± priorytetów.';
+    $Self->{Translation}->{'Admin of the CIP matrix.'} = 'Administracja macierz± KWP.';
 
     #
     # OBSOLETE ENTRIES FOR REFERENCE, DO NOT TRANSLATE!
