aa30cb324257d08a38627e1f243f34189d402561
Udo Bretz <udo.bretz@otrs.com>
2015-07-29 17:19:59 +0200

    Code cleanup.

--- var/httpd/htdocs/js/ITSM.Agent.IncidentState.js
+++ var/httpd/htdocs/js/ITSM.Agent.IncidentState.js
@@ -1,6 +1,6 @@
 // --
 // ITSM.Agent.IncidentState.js - provides the special module functions to show the service incident state
-// Copyright (C) 2001-2011 OTRS AG, http://otrs.org/\n";
+// Copyright (C) 2001-2015 OTRS AG, http://otrs.com/
 // --
 // This software comes with ABSOLUTELY NO WARRANTY. For details, see
 // the enclosed file COPYING for license information (AGPL). If you
